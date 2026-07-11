9b0743a250d49e18d034cb2469c792cae828230b
Sudha-112 <157514848+Sudha-112@users.noreply.github.com>
2026-07-11 07:58:29 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Sudha"  // name is string
+let age = 23        // age is integer
+let isLoggedIn = false   // isLoggedIn is boolean
+
+
+//Primitive Data types 
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => abhi value assign nhi hui hai 
+// symbol => unique
+
+// object 
+
+console.log(typeof name); // string
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
