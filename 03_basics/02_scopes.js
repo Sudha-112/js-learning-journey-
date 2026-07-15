83a151fd0e94a9f93ccc39c0cb0c2f6367badde6
sudha <sudhapalyal81@gmail.com>
2026-07-15 13:28:22 +0530

    global and local scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,8 +3,8 @@ let a = 300;  // global scope
 if(true){
 let a = 10;
 const b = 20;
-console.log("Inner : ", a);  //block scope
+console.log("Inner : ", a);  //block scope  o/p = 10
 }
 
-console.log(a);
+console.log(a); //  o/p = 300
 // console.log(b);
