1663750289c89109cc469e79b946a08c9c3e137a
sudha <sudhapalyal81@gmail.com>
2026-07-16 18:34:31 +0530

    Functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,3 +8,35 @@ console.log("Inner : ", a);  //block scope  o/p = 10
 
 console.log(a); //  o/p = 300
 // console.log(b);
+
+//Nested scope
+
+function one(){
+    const username = "sudha";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+
+    // console.log(website);  // error
+
+    two();
+}
+
+one();
+
+//  interesting
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1;
+}
+
+
+const addTwo = function(num){
+    return num + 1;
+}
+
+console.log(addTwo(7));
