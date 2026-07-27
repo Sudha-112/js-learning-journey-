9ff8f738bdfb23e85ac8806fb170a5aac0660234
sudha <sudhapalyal81@gmail.com>
2026-07-27 21:56:28 +0530

    updation

--- 09_OOP_and_Classes/call.js
+++ 09_OOP_and_Classes/call.js
@@ -6,6 +6,7 @@ function setUsername(username){
 }
 
 function createUser(username ,email, password){
+    console.log(this);
     setUsername.call(this, username);  // call = helps to pass current context to the other function
 
     this.email = email;
@@ -16,3 +17,6 @@ const chai = new createUser("sudha", "sudha@gmail.com", "123");
 console.log(chai);
 
 // this = refers to the current context 
+//call() = ek method hai jo function ko turant execute karta hai, 
+// saath hi this ki value manually set karne deta hai — bind() ke opposite,
+//  jo sirf naya function banata hai bina turant chalaye.
---
+++ 09_OOP_and_Classes/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function setUsername(username){
+//     // complex DB calls
+//     console.log(this);
+//     this.username = username; 
+//     console.log("called");
+// }
