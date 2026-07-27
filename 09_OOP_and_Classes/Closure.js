6ccec4a904cbbfa8f7e304efc6cec047a6dd8c90
sudha <sudhapalyal81@gmail.com>
2026-07-27 22:12:49 +0530

    updation

--- 09_OOP_and_Classes/Closure.js
+++ 09_OOP_and_Classes/Closure.js
@@ -7,4 +7,8 @@ function makeFunc(){
 }
 
 const myFunc = makeFunc();  // displayName func ke saath saath uska lexical scope bhi return hoga
-myFunc();
+myFunc();   // isme return hua displayNAme func  plus name variable bhi aata hai
+
+//Closure ka matlab hai — ek function apne "birth place" 
+// (jahan wo define hua tha) ke variables ko hamesha ke liye yaad rakh leta hai, chahe wo outer function khatam ho jaaye —
+//  kyunki function apne saath uska lexical scope bhi carry karta hai.
